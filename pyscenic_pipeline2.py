ebddc11c9733a4938c6215af206b5295df2cd63a
LiSu935 <57879262+LiSu935@users.noreply.github.com>
2023-06-01 15:26:53 -0500

    Create pyscenic_pipeline2.py

---
+++ pyscenic_pipeline2.py
@@ -0,0 +1,263 @@
+import os
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import loompy as lp
+from MulticoreTSNE import MulticoreTSNE as TSNE
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# set a working directory
+wdir = "/storage/htc/joshilab/Su_Li/Spencerlab/scenic_application/"
+os.chdir( wdir )
+
+# here to set a prefix for ease
+prefix = "c1"
+output_dir = "results/"+prefix+"/"
+
+# =====No need if already QC by Seurat================================================= #
+# ===================================================================================== #
+# path to unfiltered loom file (this will be created in the optional steps below)
+#f_loom_path_unfilt = "pbmc10k_unfiltered.loom" # test dataset, n=500 cells
+# ===================================================================================== #
+
+# path to loop file that has been QC and clustering by Seurat:
+f_anndata_path_input = "../data/c1_slim.h5ad"
+
+# path to unfiltered loom file (this will be created in the optional steps below)
+f_loom_path_unfilt = output_dir+prefix+"_unfiltered.loom"
+
+# # path to loom file with basic filtering applied (this will be created in the "initial filtering" step below). Optional.
+f_loom_path_scenic = output_dir+prefix+"_integrated.loom"
+
+# path to anndata object, which will be updated to store Scanpy results as they are generated below
+# replaced with h5ad file converted from Seurat, since it contains the scale.data and the clustering results as well
+f_anndata_path = "../data/c1_integrated.h5ad"
+
+# path to pyscenic output
+f_pyscenic_output = output_dir+prefix+"_pyscenic_output.loom"
+
+# loom output, generated from a combination of Scanpy and pySCENIC results:
+f_final_loom = output_dir+prefix+'_scenic_integrated-output.loom'
+
+
+
+adata = sc.read_h5ad( f_anndata_path )
+
+adata.obs['seurat_clusters'] = adata.obs['seurat_clusters'].astype("category")
+
+sc.pl.umap(adata, color=['seurat_clusters'], save=output_dir+prefix+'_seurat_clusters.png' )
+
+# find marker genes
+sc.tl.rank_genes_groups(adata, 'seurat_clusters', method='t-test', use_raw=False)
+sc.pl.rank_genes_groups(adata, n_genes=25, sharey=False)
+
+# sc.tl.rank_genes_groups(adata, 'louvain', method='logreg')
+# sc.pl.rank_genes_groups(adata, n_genes=25, sharey=False)
+pd.DataFrame(adata.uns['rank_genes_groups']['names']).head(10)
+
+# tSNE
+tsne = TSNE( n_jobs=20 )
+adata.obsm['X_tsne'] = tsne.fit_transform( adata.X )
+adata.write( f_anndata_path )
+
+nGenesDetectedPerCellbefore = np.sum(adata.X>0, axis=1)
+nGenesDetectedPerCell = pd.Series(nGenesDetectedPerCellbefore)
+percentiles = nGenesDetectedPerCell.quantile([0.01, 0.05, 0.10, 0.50, 1])
+print(percentiles)
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 5), dpi=150)
+sns.distplot(nGenesDetectedPerCell, norm_hist=False, kde=False, bins='fd')
+for i,x in enumerate(percentiles):
+    fig.gca().axvline(x=x, ymin=0,ymax=1, color='red')
+    ax.text(x=x, y=ax.get_ylim()[1], s=f'{int(x)} ({percentiles.index.values[i]*100}%)', color='red', rotation=30, size='x-small',rotation_mode='anchor' )
+ax.set_xlabel('# of genes')
+ax.set_ylabel('# of cells')
+fig.tight_layout()
+fig.savefig(output_dir+prefix+'_nGeneDetectedPerCell.png')
+
+import json
+import zlib
+import base64
+
+# collect SCENIC AUCell output
+lf = lp.connect( f_pyscenic_output, mode='r+', validate=False )
+auc_mtx = pd.DataFrame( lf.ca.RegulonsAUC, index=lf.ca.CellID)
+lf.close()
+
+import umap
+from tqdm import tqdm
+
+# UMAP
+runUmap = umap.UMAP(n_neighbors=10, min_dist=0.4, metric='correlation').fit_transform
+dr_umap = runUmap( auc_mtx )
+pd.DataFrame(dr_umap, columns=['X', 'Y'], index=auc_mtx.index).to_csv( output_dir+prefix+"_scenic_umap.txt", sep='\t')
+# tSNE
+tsne = TSNE( n_jobs=20 )
+dr_tsne = tsne.fit_transform( auc_mtx )
+pd.DataFrame(dr_tsne, columns=['X', 'Y'], index=auc_mtx.index).to_csv( output_dir+prefix+"_scenic_tsne.txt", sep='\t')
+
+# scenic output
+lf = lp.connect( f_pyscenic_output, mode='r+', validate=False )
+meta = json.loads(zlib.decompress(base64.b64decode( lf.attrs.MetaData )))
+#exprMat = pd.DataFrame( lf[:,:], index=lf.ra.Gene, columns=lf.ca.CellID)
+auc_mtx = pd.DataFrame( lf.ca.RegulonsAUC, index=lf.ca.CellID)
+regulons = lf.ra.Regulons
+dr_umap = pd.read_csv( output_dir+prefix+"_scenic_umap.txt", sep='\t', header=0, index_col=0 )
+dr_tsne = pd.read_csv( output_dir+prefix+"_scenic_tsne.txt", sep='\t', header=0, index_col=0 )
+###
+
+auc_mtx.columns = auc_mtx.columns.str.replace('\(','_(')
+regulons.dtype.names = tuple( [ x.replace("(","_(") for x in regulons.dtype.names ] )
+# regulon thresholds
+rt = meta['regulonThresholds']
+for i,x in enumerate(rt):
+    tmp = x.get('regulon').replace("(","_(")
+    x.update( {'regulon': tmp} )
+    
+tsneDF = pd.DataFrame(adata.obsm['X_tsne'], columns=['_X', '_Y'])
+
+Embeddings_X = pd.DataFrame( index=lf.ca.CellID )
+Embeddings_X = pd.concat( [
+        pd.DataFrame(adata.obsm['X_umap'],index=adata.obs.index)[0] ,
+        pd.DataFrame(adata.obsm['X_pca'],index=adata.obs.index)[0] ,
+        dr_tsne['X'] ,
+        dr_umap['X']
+    ], sort=False, axis=1, join='outer' )
+Embeddings_X.columns = ['1','2','3','4']
+
+Embeddings_Y = pd.DataFrame( index=lf.ca.CellID )
+Embeddings_Y = pd.concat( [
+        pd.DataFrame(adata.obsm['X_umap'],index=adata.obs.index)[1] ,
+        pd.DataFrame(adata.obsm['X_pca'],index=adata.obs.index)[1] ,
+        dr_tsne['Y'] ,
+        dr_umap['Y']
+    ], sort=False, axis=1, join='outer' )
+Embeddings_Y.columns = ['1','2','3','4']
+
+### metadata
+metaJson = {}
+
+metaJson['embeddings'] = [
+    {
+        "id": -1,
+        "name": f"Scanpy t-SNE (highly variable genes)"
+    },
+    {
+        "id": 1,
+        "name": f"Scanpy UMAP  (highly variable genes)"
+    },
+    {
+        "id": 2,
+        "name": "Scanpy PC1/PC2"
+    },
+    {
+        "id": 3,
+        "name": "SCENIC AUC t-SNE"
+    },
+    {
+        "id": 4,
+        "name": "SCENIC AUC UMAP"
+    },
+]
+
+metaJson["clusterings"] = [{
+            "id": 0,
+            "group": "Scanpy",
+            "name": "Seurat louvain default resolution",
+            "clusters": [],
+        }]
+
+metaJson["metrics"] = [
+        {
+            "name": "nUMI"
+        }, {
+            "name": "nGene"
+        }, {
+            "name": "Percent_mito"
+        }
+]
+
+metaJson["annotations"] = [
+    {
+        "name": "Louvain_clusters_Scanpy",
+        "values": list(set( adata.obs['seurat_clusters'].astype(np.str) ))
+    },
+    #{
+    #    "name": "Genotype",
+    #    "values": list(set(adata.obs['Genotype'].values))
+    #},
+    #{
+    #    "name": "Timepoint",
+    #    "values": list(set(adata.obs['Timepoint'].values))
+    #},
+    #{
+    #    "name": "Sample",
+    #    "values": list(set(adata.obs['Sample'].values))
+    #}
+]
+
+# SCENIC regulon thresholds:
+metaJson["regulonThresholds"] = rt
+
+for i in range(max(set([int(x) for x in adata.obs['seurat_clusters']])) + 1):
+    clustDict = {}
+    clustDict['id'] = i
+    clustDict['description'] = f'Unannotated Cluster {i + 1}'
+    metaJson['clusterings'][0]['clusters'].append(clustDict)
+    
+clusterings = pd.DataFrame()
+clusterings["0"] = adata.obs['seurat_clusters'].values.astype(np.int64)
+
+# Assemble loom file row and column attributes
+def dfToNamedMatrix(df):
+    arr_ip = [tuple(i) for i in df.values]
+    dtyp = np.dtype(list(zip(df.dtypes.index, df.dtypes)))
+    arr = np.array(arr_ip, dtype=dtyp)
+    return arr
+
+col_attrs = {
+    "CellID": np.array(adata.obs.index),
+    "nUMI": np.array(adata.obs['nCount_SCT'].values),
+    "nGene": np.array(adata.obs['nFeature_SCT'].values),
+    "Louvain_clusters_Scanpy": np.array( adata.obs['seurat_clusters'].values ),
+    #"Genotype": np.array(adata.obs['Genotype'].values),
+    #"Timepoint": np.array(adata.obs['Timepoint'].values),
+    #"Sample": np.array(adata.obs['Sample'].values),
+    "Percent_mito": np.array(adata.obs['percent_mito'].values),
+    "Embedding": dfToNamedMatrix(tsneDF),
+    "Embeddings_X": dfToNamedMatrix(Embeddings_X),
+    "Embeddings_Y": dfToNamedMatrix(Embeddings_Y),
+    "RegulonsAUC": dfToNamedMatrix(auc_mtx),
+    "Clusterings": dfToNamedMatrix(clusterings),
+    "ClusterID": np.array(adata.obs['seurat_clusters'].values)
+}
+
+row_attrs = {
+    "Gene": lf.ra.Gene,
+    "Regulons": regulons,
+}
+
+attrs = {
+    "title": "sampleTitle",
+    "MetaData": json.dumps(metaJson),
+    "Genome": 'hg38',
+    "SCopeTreeL1": "",
+    "SCopeTreeL2": "",
+    "SCopeTreeL3": ""
+}
+
+# compress the metadata field:
+attrs['MetaData'] = base64.b64encode(zlib.compress(json.dumps(metaJson).encode('ascii'))).decode('ascii')
+
+lp.create(
+    filename = f_final_loom ,
+    layers=lf[:,:],
+    row_attrs=row_attrs, 
+    col_attrs=col_attrs, 
+    file_attrs=attrs
+)
+lf.close() # close original pyscenic loom file
+# This loom file can now be imported into SCope.
+
+# Then go to post-SCENIC analytics
