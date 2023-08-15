ad66d34e8c6d82ecda688885fe06de5549e4c2fa
LiSu935 <57879262+LiSu935@users.noreply.github.com>
2023-08-15 17:03:08 -0500

    Create Step4_run_post_scenic_original.sh

---
+++ Step4_run_post_scenic_original.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+#  SBATCH CONFIG
+#-------------------------------------------------------------------------------
+## resources
+#SBATCH --job-name=scenic_withoutDask-%j.out
+#SBATCH --output=scenic_hsc-%j.out  # %j is the unique jobID
+echo "### Starting at: $(date) ###"
+
+source activate pyscenic37
+
+prefix='c_pnd_combined'
+working_dir='/scratch/lisu/Spencerlab/scenic_application/'
+path_ad_file_from_seurat_integrated="/scratch/lisu/Spencerlab/scenic_application/seurat_scenicInput/c_pnd_combined_integrated.h5ad"
+
+
+#STEP 0:
+python /scratch/lisu/lisu_git/scenic_application/post_scenic_original.py --prefix ${prefix} --wdir ${working_dir} --path_ad_file_from_seurat_integrated ${path_ad_file_from_seurat_integrated} 
+
+echo "step 0 finished"
